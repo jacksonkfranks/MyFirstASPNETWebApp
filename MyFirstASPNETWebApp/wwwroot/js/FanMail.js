e31c0c87306137230242afd5cfb771e0c10bdfc7
jacksonkfranks <73501456+jacksonkfranks@users.noreply.github.com>
2022-01-09 19:44:44 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,13 @@
+﻿////document.getElementById("btnSend").addEventListener("click", function () {
+////    alert("From: " + document.getElementById("txtFrom").value +
+////        ", Subject: " + document.getElementById("txtFrom").value +
+////        ", Message: " + document.getElementById("txtMessage").value);
+////})
+
+$("#btnSend").click( function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtFrom").val() +
+        ", Message: " + $("#txtMessage").val());
+
+    $("#picGanderson").fadeOut("slow");
+})
